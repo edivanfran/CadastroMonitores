faeb1223425c1741112510c6e58a9e612c626cc2
theduarda <eduardacorreia1414@gmail.com>
2025-11-11 15:39:36 -0300

    Adicionar classe Coordenador

--- src/Coordenador.java
+++ src/Coordenador.java
@@ -1,2 +1,26 @@
+
 public class Coordenador {
+    private String email;
+    private String senha;
+
+    public Coordenador(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }

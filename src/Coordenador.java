94cf293e6be0fc0a6281d495b776ac6e022e5fac
theduarda <eduardacorreia1414@gmail.com>
2025-11-28 17:18:55 -0300

    Implementação de metodos na classe Coordenador / herdar usuario

--- src/Coordenador.java
+++ src/Coordenador.java
@@ -1,39 +1,56 @@
+import java.util.ArrayList;
 
-public class Coordenador {
-    private String nome;
-    private String email;
-    private String senha;
+public class Coordenador extends Usuario {
 
     public Coordenador(String email, String senha) {
-        this.email = email;
-        this.senha = senha;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
+        super(email, senha);
     }
 
     public boolean autenticar(String email, String senha) {
         return this.email.equals(email) && this.senha.equals(senha);
+    }
+
+    /**
+     * Cadastra um novo edital de monitoria no sistema.
+     * central A central de informações onde o edital será cadastrado
+     * @param edital O edital a ser cadastrado
+     * @return true se o edital foi cadastrado com sucesso, false caso contrário
+     */
+    public boolean cadastrarEdital(CentralDeInformacoes central, EditalDeMonitoria edital) {
+        return central.cadastrarEdital(this, edital);
+    }
+
+    /**
+     * Calcula o resultado (ranqueamento) de um edital.
+     * @param edital O edital cujo resultado será calculado
+     * @return true se o cálculo foi realizado com sucesso, false caso contrário
+     */
+    public boolean calcularResultadoEdital(EditalDeMonitoria edital) {
+        return edital.calcularResultado(this);
+    }
+
+    /**
+     * Fecha um edital, impedindo novas inscrições.
+     * @param edital O edital a ser fechado
+     * @return true se o edital foi fechado com sucesso, false caso contrário
+     */
+    public boolean fecharEdital(EditalDeMonitoria edital) {
+        return edital.fecharEdital(this);
+    }
+
+    /**
+     * Lista todos os alunos cadastrados no sistema.
+     * @param central A central de informações
+     * @return Lista de alunos cadastrados, ou null em caso de erro
+     */
+    public ArrayList<Aluno> listarAlunos(CentralDeInformacoes central) {
+        return central.listarAlunos(this);
+    }
+    public boolean fazerLogin(String email, String senha) {
+        return super.fazerLogin(email, senha);
+    }
+
+    public void editarPerfil(String novoEmail, String novaSenha) {
+        super.editarPerfil(novoEmail, novaSenha);
     }
 }

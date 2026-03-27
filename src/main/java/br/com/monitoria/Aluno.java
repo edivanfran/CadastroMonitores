bfa8faa541e1982bb10391557000d9e033fbbbdd
Edivan <edivanfrancisco437@gmail.com>
2026-03-27 16:03:30 -0300

    feat(Aluno): adiciona mapeamento de Aluno
    - adiciona o construtor vazio para o JPA
    - adiciona método abrirTelaCadastroEdital

--- src/main/java/br/com/monitoria/Aluno.java
+++ src/main/java/br/com/monitoria/Aluno.java
@@ -1,11 +1,24 @@
 package br.com.monitoria;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 /**
  * Representa um usuário que tenha sido cadastrado numa central de informações como aluno. Possui e-mail, senha, nome e gênero.
  * Possui também um número de matrícula, que serve como identificador exclusivo do aluno.
  */
+@Entity
+@DiscriminatorValue("A")
 public class Aluno extends Usuario{
+	@Column(unique = true, nullable = false)
 	public String matricula;
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
 	public Sexo genero;
 
     public String getMatricula() {
@@ -16,6 +29,10 @@ public class Aluno extends Usuario{
     }
     public void setGenero(Sexo genero) {
         this.genero = genero;
+    }
+
+    protected Aluno() {
+        // Construtor para JPA
     }
 
 
@@ -32,5 +49,11 @@ public class Aluno extends Usuario{
 	public String toString() {
 		return this.getNome();
 	}
+
+    @Override
+    public void abrirTelaCadastroEdital(JFrame telaPai) {
+        // O Aluno NÃO PODE. Então ele mostra uma mensagem de erro.
+        JOptionPane.showMessageDialog(telaPai, "Apenas coordenadores podem cadastrar editais.", "Acesso Negado", JOptionPane.WARNING_MESSAGE);
+    }
 
 }

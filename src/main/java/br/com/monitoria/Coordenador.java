3753e46c5d40c25f40857ebaa5e4623883401acb
Edivan <edivanfrancisco437@gmail.com>
2026-03-27 14:45:21 -0300

    feat(coordenador): implementa o mapeamento da classe Coordenador
    - Adiciona o construtor de coordenador para o JPA

--- src/main/java/br/com/monitoria/Coordenador.java
+++ src/main/java/br/com/monitoria/Coordenador.java
@@ -1,18 +1,24 @@
 package br.com.monitoria;
 
-import java.util.ArrayList;
-import br.com.monitoria.excecoes.*;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
 
 /**
  * <p>Representa o usuário atribuído na central de informações como o Coordenador do Curso.
  * Possui e-mail, senha e nome, mas não precisa de matrícula.</p>
  * <p>Só pode haver um Coordenador em um mesmo objeto {@link CentralDeInformacoes}.</p>
  */
+
+@Entity
+@DiscriminatorValue("C")
 public class Coordenador extends Usuario {
 
     public Coordenador(String email, String senha, String nome) {
         super(email, senha, nome);
     }
 
+    protected Coordenador() {
+        // Construtor para JPA
+    }
 
 }

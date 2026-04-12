242692ddd0fcf2365793e21f9bf1fcfda61fd95a
Edivan <edivanfrancisco437@gmail.com>
2026-04-11 22:49:43 -0300

    refactor(TelaCadastrarEdital): remove o acoplamento com CentralDeInformacoes, Persistencia e nomeDeArquivo
    - Altera o construtor para receber dependências e substitui o uso direto de CentralDeInformacoes por GerenciadorDeDados, desacoplando lógica e facilitando testes.

--- src/main/java/br/com/monitoria/TelaCadastrarEdital.java
+++ src/main/java/br/com/monitoria/TelaCadastrarEdital.java
@@ -15,9 +15,9 @@ public class TelaCadastrarEdital extends TelaEditalBase {
     private JButton botaoSalvar;
     private JButton botaoVoltar;
 
-    public TelaCadastrarEdital(CentralDeInformacoes central, Persistencia persistencia, String nomeArquivo) {
+    public TelaCadastrarEdital() {
         // Passa null porque não tem um edital cadastrado.
-        super("Cadastrar Novo Edital", null, central, persistencia, nomeArquivo);
+        super("Cadastrar Novo Edital", null);
     }
 
     protected void criarComponentes() {
@@ -90,10 +90,7 @@ public class TelaCadastrarEdital extends TelaEditalBase {
                         (Double) pesoNota.getValue()
                 );
 
-                getCentral().adicionarEdital(novoEdital);
-                getPersistencia().salvarCentral(getCentral(), getNomeArquivo());
-                // Notifica a TelaPrincipal
-                getCentral().notificarObservadores();
+                gerenciadorDeDados.salvarEdital(novoEdital);
                 mostrarSucesso("Edital cadastrado com sucesso!");
 
                 voltarParaTelaPrincipal();

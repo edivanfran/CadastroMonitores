be5bb63a918bd443020c4aa9a9884cb8e5288dad
Edivan <edivanfrancisco437@gmail.com>
2026-04-13 12:07:27 -0300

    refactor(Main): limpa as dependencias de Central, Persistencia e nomeArquivo

--- src/main/java/br/com/monitoria/Main.java
+++ src/main/java/br/com/monitoria/Main.java
@@ -33,13 +33,8 @@ public class Main {
             System.out.println("Boa madrugada.");
         }
 
-        Persistencia persistencia = new Persistencia();
-        CentralDeInformacoes central;
-        String nomeArquivo;
         if (arquivos.isEmpty()) {
             System.out.println("Não foi encontrado nenhum arquivo de Central de Informações de Alunos.\nCriando nova Central...");
-            central = new CentralDeInformacoes();
-            nomeArquivo = "central".toUpperCase();
         } else {
             System.out.println("Foram encontradas as seguintes Centrais de Informações de Alunos:\n-------------------------------");
             for (File arquivo : arquivos) {
@@ -47,11 +42,8 @@ public class Main {
             }
             System.out.println("-------------------------------");
             System.out.print("Espere um momento estamos pegar arquivo da nuvem » ");
-            nomeArquivo = "central".toUpperCase();
-            central = persistencia.recuperarCentral(nomeArquivo);
         }
 
         InicializadorGUI.iniciar();
-
     }
 }

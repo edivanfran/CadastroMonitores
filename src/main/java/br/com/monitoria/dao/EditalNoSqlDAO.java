f80ca0a40ee2e1a82d5edbe239f9151aca709512
Edivan <edivanfrancisco437@gmail.com>
2026-05-29 15:04:53 -0300

    feat(EditalNoSqlDAO): Adicionar lógico dos métodos NoSQL
    - Adicionar lógica dos métodos, salvar, atualizar, excluir, buscarPorId e retornarTodos

--- src/main/java/br/com/monitoria/dao/EditalNoSqlDAO.java
+++ src/main/java/br/com/monitoria/dao/EditalNoSqlDAO.java
@@ -1,33 +1,124 @@
 package br.com.monitoria.dao;
 
+import br.com.monitoria.model.Disciplina;
 import br.com.monitoria.model.EditalDeMonitoria;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.ReplaceOptions;
+import org.bson.Document;
+import org.bson.types.ObjectId;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class EditalNoSqlDAO implements DAO<EditalDeMonitoria, Long>{
+public class EditalNoSqlDAO implements DAO<EditalDeMonitoria, String> {
 
-    @Override
-    public void salvar(EditalDeMonitoria editalDeMonitoria) {
+    private final MongoCollection<Document> editalCollection;
+    private final MongoClient mongoClient;
 
+    public EditalNoSqlDAO(MongoClient mongoClient) {
+        this.mongoClient = mongoClient;
+        MongoDatabase database = mongoClient.getDatabase("bancomonitores");
+        this.editalCollection = database.getCollection("editais");
     }
 
     @Override
-    public void atualizar(EditalDeMonitoria editalDeMonitoria) {
-
+    public void salvar(EditalDeMonitoria edital) {
+        Document editalDoc = toDocument(edital);
+        editalCollection.insertOne(editalDoc);
+        edital.setId(editalDoc.getObjectId("_id").toString());
     }
 
     @Override
-    public void excluir(EditalDeMonitoria editalDeMonitoria) {
-
+    public void atualizar(EditalDeMonitoria edital) {
+        if (edital.getId() == null) {
+            throw new IllegalArgumentException("O ID do edital não pode ser nulo para atualização.");
+        }
+        Document editalDoc = toDocument(edital);
+        ObjectId objectId = new ObjectId(edital.getId());
+        editalCollection.replaceOne(Filters.eq("_id", objectId), editalDoc, new ReplaceOptions().upsert(true));
     }
 
     @Override
-    public EditalDeMonitoria buscarPorId(Long aLong) {
-        return null;
+    public void excluir(EditalDeMonitoria edital) {
+        if (edital.getId() == null) {
+            throw new IllegalArgumentException("O ID do edital não pode ser nulo para exclusão.");
+        }
+        editalCollection.deleteOne(Filters.eq("_id", new ObjectId(edital.getId())));
+    }
+
+    @Override
+    public EditalDeMonitoria buscarPorId(String id) {
+        Document doc = editalCollection.find(Filters.eq("_id", new ObjectId(id))).first();
+        return (doc != null) ? fromDocument(doc) : null;
     }
 
     @Override
     public List<EditalDeMonitoria> retornarTodos() {
-        return List.of();
+        List<EditalDeMonitoria> editais = new ArrayList<>();
+        try (MongoCursor<Document> cursor = editalCollection.find().iterator()) {
+            while (cursor.hasNext()) {
+                Document doc = cursor.next();
+                editais.add(fromDocument(doc));
+            }
+        }
+        return editais;
+    }
+
+    private Document toDocument(EditalDeMonitoria edital) {
+        List<Document> disciplinasDocs = edital.getDisciplinas().stream()
+                .map(d -> new Document("nome", d.getNomeDisciplina())
+                        .append("vagas_remuneradas", d.getVagasRemuneradas())
+                        .append("vagas_voluntarias", d.getVagasVoluntarias()))
+                .collect(Collectors.toList());
+
+        Document doc = new Document()
+                .append("numero", edital.getNumero())
+                .append("data_inicio", edital.getDataInicio().toString())
+                .append("data_limite", edital.getDataLimite().toString())
+                .append("disciplinas", disciplinasDocs)
+                .append("aberto", edital.isAberto())
+                .append("peso_cre", edital.getPesoCre())
+                .append("peso_nota", edital.getPesoNota())
+                .append("resultado_calculado", edital.isResultadoCalculado())
+                .append("periodo_desistencia_encerrado", edital.isPeriodoDesistenciaEncerrado());
+
+        if (edital.getId() != null) {
+            doc.put("_id", new ObjectId(edital.getId()));
+        }
+
+        return doc;
+    }
+
+    private EditalDeMonitoria fromDocument(Document doc) {
+        if (doc == null) {
+            return null;
+        }
+
+        List<Document> disciplinasDocs = doc.getList("disciplinas", Document.class, new ArrayList<>());
+        List<Disciplina> disciplinas = disciplinasDocs.stream()
+                .map(d -> new Disciplina(
+                        d.getString("nome"),
+                        d.getInteger("vagas_voluntarias", 0),
+                        d.getInteger("vagas_remuneradas", 0)))
+                .collect(Collectors.toList());
+
+        return new EditalDeMonitoria(
+                doc.getObjectId("_id").toString(),
+                doc.getString("numero"),
+                LocalDate.parse(doc.getString("data_inicio")),
+                LocalDate.parse(doc.getString("data_limite")),
+                disciplinas,
+                doc.getBoolean("aberto", false),
+                doc.getDouble("peso_cre"),
+                doc.getDouble("peso_nota"),
+                doc.getBoolean("resultado_calculado", false),
+                doc.getBoolean("periodo_desistencia_encerrado", false)
+        );
     }
 }

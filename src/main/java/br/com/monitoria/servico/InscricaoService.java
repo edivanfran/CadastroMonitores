65a719da25576903f19ce9718a3fa297163774f5
Edivan <edivanfrancisco437@gmail.com>
2026-06-08 16:01:58 -0300

    feat(InscricaoService): cria service para a TelaInscreverEmEditalAluno
    - Desacoplar a lógica de negócio da TelaInscreverEmEditalAluno

---
+++ src/main/java/br/com/monitoria/servico/InscricaoService.java
@@ -0,0 +1,62 @@
+package br.com.monitoria.servico;
+
+import br.com.monitoria.GerenciadorDeDados;
+import br.com.monitoria.PreferenciaInscricao;
+import br.com.monitoria.SessaoUsuario;
+import br.com.monitoria.Vaga;
+import br.com.monitoria.excecoes.VagasEsgotadasException;
+import br.com.monitoria.model.Aluno;
+import br.com.monitoria.model.Disciplina;
+import br.com.monitoria.model.EditalDeMonitoria;
+
+public class InscricaoService {
+
+    private GerenciadorDeDados gerenciadorDeDados;
+    private SessaoUsuario sessaoUsuario;
+
+    public InscricaoService() {
+        this.gerenciadorDeDados = GerenciadorDeDados.getInstancia();
+        this.sessaoUsuario = SessaoUsuario.getInstancia();
+    }
+
+    public String inscreverAluno(EditalDeMonitoria edital, Disciplina disciplina, double cre, double nota, int ordemPreferencia, PreferenciaInscricao preferenciaVaga) throws Exception {
+        if (disciplina == null) {
+            throw new Exception("Por favor, selecione uma disciplina.");
+        }
+
+        Aluno alunoLogado = (Aluno) sessaoUsuario.getUsuarioLogado();
+        String mensagemSucesso = "";
+
+        switch (preferenciaVaga) {
+            case SOMENTE_REMUNERADA:
+                realizarInscricao(edital, alunoLogado, disciplina, cre, nota, Vaga.REMUNERADA, ordemPreferencia, preferenciaVaga);
+                mensagemSucesso = "Inscrição para vaga REMUNERADA realizada com sucesso!";
+                break;
+            case SOMENTE_VOLUNTARIA:
+                realizarInscricao(edital, alunoLogado, disciplina, cre, nota, Vaga.VOLUNTARIA, ordemPreferencia, preferenciaVaga);
+                mensagemSucesso = "Inscrição para vaga VOLUNTARIA realizada com sucesso!";
+                break;
+            case REMUNERADA_OU_VOLUNTARIA:
+                try {
+                    // Tenta primeiro a vaga remunerada
+                    realizarInscricao(edital, alunoLogado, disciplina, cre, nota, Vaga.REMUNERADA, ordemPreferencia, preferenciaVaga);
+                    mensagemSucesso = "Inscrição para vaga REMUNERADA realizada com sucesso!";
+                } catch (VagasEsgotadasException eRemunerada) {
+                    // Se não conseguiu, tenta a voluntária
+                    try {
+                        realizarInscricao(edital, alunoLogado, disciplina, cre, nota, Vaga.VOLUNTARIA, ordemPreferencia, preferenciaVaga);
+                        mensagemSucesso = "Vagas remuneradas esgotadas. Inscrição para vaga VOLUNTARIA realizada com sucesso!";
+                    } catch (VagasEsgotadasException eVoluntaria) {
+                        // Se também não conseguiu, informa o erro
+                        throw new Exception("Não há mais vagas remuneradas ou voluntárias para esta disciplina.");
+                    }
+                }
+                break;
+        }
+        return mensagemSucesso;
+    }
+
+    private void realizarInscricao(EditalDeMonitoria edital, Aluno aluno, Disciplina disciplina, double cre, double nota, Vaga tipoVaga, int ordem, PreferenciaInscricao pref) throws Exception {
+        gerenciadorDeDados.inscreverAlunoEmEdital(edital, aluno, disciplina, cre, nota, tipoVaga, ordem, pref);
+    }
+}

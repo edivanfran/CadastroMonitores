0116f7d09bd3f99103dc5db7f5b067b782ee01e4
Edivan <edivanfrancisco437@gmail.com>
2026-06-08 21:37:28 -0300

    feat(InscricaoService): melhora comunicação e envio de exceções
    - Adiciona tratamento de erros

--- src/main/java/br/com/monitoria/servico/InscricaoService.java
+++ src/main/java/br/com/monitoria/servico/InscricaoService.java
@@ -4,6 +4,7 @@ import br.com.monitoria.GerenciadorDeDados;
 import br.com.monitoria.PreferenciaInscricao;
 import br.com.monitoria.SessaoUsuario;
 import br.com.monitoria.Vaga;
+import br.com.monitoria.excecoes.ValidacaoException;
 import br.com.monitoria.excecoes.VagasEsgotadasException;
 import br.com.monitoria.model.Aluno;
 import br.com.monitoria.model.Disciplina;
@@ -21,11 +22,11 @@ public class InscricaoService {
 
     public String inscreverAluno(EditalDeMonitoria edital, Disciplina disciplina, double cre, double nota, int ordemPreferencia, PreferenciaInscricao preferenciaVaga) throws Exception {
         if (disciplina == null) {
-            throw new Exception("Por favor, selecione uma disciplina.");
+            throw new ValidacaoException("Por favor, selecione uma disciplina.");
         }
 
         Aluno alunoLogado = (Aluno) sessaoUsuario.getUsuarioLogado();
-        String mensagemSucesso = "";
+        String mensagemSucesso;
 
         switch (preferenciaVaga) {
             case SOMENTE_REMUNERADA:
@@ -34,24 +35,23 @@ public class InscricaoService {
                 break;
             case SOMENTE_VOLUNTARIA:
                 realizarInscricao(edital, alunoLogado, disciplina, cre, nota, Vaga.VOLUNTARIA, ordemPreferencia, preferenciaVaga);
-                mensagemSucesso = "Inscrição para vaga VOLUNTARIA realizada com sucesso!";
+                mensagemSucesso = "Inscrição para vaga VOLUNTÁRIA realizada com sucesso!";
                 break;
             case REMUNERADA_OU_VOLUNTARIA:
                 try {
-                    // Tenta primeiro a vaga remunerada
                     realizarInscricao(edital, alunoLogado, disciplina, cre, nota, Vaga.REMUNERADA, ordemPreferencia, preferenciaVaga);
                     mensagemSucesso = "Inscrição para vaga REMUNERADA realizada com sucesso!";
                 } catch (VagasEsgotadasException eRemunerada) {
-                    // Se não conseguiu, tenta a voluntária
                     try {
                         realizarInscricao(edital, alunoLogado, disciplina, cre, nota, Vaga.VOLUNTARIA, ordemPreferencia, preferenciaVaga);
-                        mensagemSucesso = "Vagas remuneradas esgotadas. Inscrição para vaga VOLUNTARIA realizada com sucesso!";
+                        mensagemSucesso = "Vagas remuneradas esgotadas. Inscrição para vaga VOLUNTÁRIA realizada com sucesso!";
                     } catch (VagasEsgotadasException eVoluntaria) {
-                        // Se também não conseguiu, informa o erro
-                        throw new Exception("Não há mais vagas remuneradas ou voluntárias para esta disciplina.");
+                        throw new VagasEsgotadasException("Não há mais vagas remuneradas ou voluntárias para esta disciplina.");
                     }
                 }
                 break;
+            default:
+                throw new IllegalStateException("Opção de preferência de vaga inesperada: " + preferenciaVaga);
         }
         return mensagemSucesso;
     }
